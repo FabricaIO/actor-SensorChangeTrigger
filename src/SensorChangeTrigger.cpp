2f4bd88446bda9a72f3fa523685dc9f5815f2b71
Sam Groveman <samgroveman@outlook.com>
2025-11-04 11:38:59 -0500

    Refactor used class name

--- src/SensorChangeTrigger.cpp
+++ src/SensorChangeTrigger.cpp
@@ -12,8 +12,7 @@ SensorChangeTrigger::SensorChangeTrigger(String Name, String configFile) : Perio
 /// @brief Starts a sensor trigger 
 /// @return True on success
 bool SensorChangeTrigger::begin() {
-	sensor_value.parameter_config.Enabled = true;
-	sensor_value.parameter_config.Parameters.resize(1);
+	sensor_value.Parameters.resize(1);
 	// Check if file exists
 	bool configExists = checkConfig(config_path);
 	// Create settings directory if necessary
@@ -33,7 +32,7 @@ bool SensorChangeTrigger::begin() {
 
 /// @brief Receives an action
 /// @param action The action to process
-/// @param payload Payload to be passed to triggered action
+/// @param payload Payload to be passed to the triggered action
 /// @return JSON response with OK
 std::tuple<bool, String> SensorChangeTrigger::receiveAction(int action, String payload) {
 	if (action == forceTrigger) {
@@ -77,7 +76,7 @@ bool SensorChangeTrigger::setConfig(String config, bool save) {
 		int colon;
 		if ((colon = sensor_combined.indexOf(':')) != -1) {
 			std::pair<String, String> chosen {sensor_combined.substring(0, colon), sensor_combined.substring(colon + 1)};
-			sensor_value.parameter_config.Parameters[0] = chosen;
+			sensor_value.Parameters[0] = chosen;
 		}
 		sensor_change_config.valueDeltaMin = doc["valueDeltaMin"].as<double>();
 		if (save) {
@@ -94,7 +93,7 @@ bool SensorChangeTrigger::setConfig(String config, bool save) {
 bool SensorChangeTrigger::triggerAction(String payload) {
 	if (POSTSuccess) {
 		std::map<String, std::map<String, double>> params = sensor_value.getParameterValues();
-		double value = params[sensor_value.parameter_config.Parameters[0].first][sensor_value.parameter_config.Parameters[0].second];
+		double value = params[sensor_value.Parameters[0].first][sensor_value.Parameters[0].second];
 		double delta = std::abs(old_value - value);
 		if (delta >= sensor_change_config.valueDeltaMin) {
 			old_value = value;
@@ -121,7 +120,7 @@ JsonDocument SensorChangeTrigger::addAdditionalConfig() {
 		return doc;
 	}
 	// Add all sensor options to dropdown
-	doc["sensorParameter"]["current"] = sensor_value.parameter_config.Parameters.size() > 0 ? sensor_value.parameter_config.Parameters[0].first + ":" + sensor_value.parameter_config.Parameters[0].second : "";
+	doc["sensorParameter"]["current"] = sensor_value.Parameters.size() > 0 ? sensor_value.Parameters[0].first + ":" + sensor_value.Parameters[0].second : "";
 	std::map<String, std::vector<String>> sensors = sensor_value.listAllParameters();
 	int i = 0;
 	if (sensors.size() > 0) {
--- src/SensorChangeTrigger.h
+++ src/SensorChangeTrigger.h
@@ -10,7 +10,7 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <PeriodicActionTrigger.h>
-#include <ParameterTrigger.h>
+#include <ParameterGetter.h>
 
 /// @brief Class providing action triggers based on sensor readings
 class SensorChangeTrigger : public PeriodicActionTrigger {
@@ -28,7 +28,7 @@ class SensorChangeTrigger : public PeriodicActionTrigger {
 		double old_value = 0;
 
 		/// @brief Provides access to sensor value
-		ParameterTrigger sensor_value;
+		ParameterGetter sensor_value;
 
 		/// @brief Index for action that Checks if the sensor should trigger the action
 		int forceTrigger;
